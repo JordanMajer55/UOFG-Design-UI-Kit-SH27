cb725c6a58fbeb30ba2ac64682b9f54d1588aec8
”Otgonbayar <2963377b@student.gla.ac.uk>
2025-11-23 13:41:11 +0000

    Change the static test IP server to localhost

--- app/client_testing/test_client.ts
+++ app/client_testing/test_client.ts
@@ -1,5 +1,5 @@
 async function test() {
-    const response = await fetch("http://172.21.160.1:3000/");
+    const response = await fetch("http://localhost:3000/");
     const data = await response.json();
     console.log(data.message)
 }
