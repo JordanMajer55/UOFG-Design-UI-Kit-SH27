1ff134b6f8b9257e9125fa689f9a1a04c71a8d8d
PhoenixNels <phoenixnelson05@gmail.com>
2025-12-03 13:03:04 +0000

    Webhook endpoint created on app.js

--- app/app.js
+++ app/app.js
@@ -2,10 +2,20 @@ var express = require('express');
 var app = express();
 var figma_port = 3000;
 var client_port = 3001;
+
 app.get('/', function (req, res) {
     res.send({ "message": 'Hello World!' });
 });
+
+//webhook endpoint 
+app.post('/webhook', (req, res) => {
+    console.log("Webhook received:", req.body);
+    res.sendStatus(200);
+});
+
+
 app.listen(figma_port, function () {
+    console.log("Server running...");
 });
 app.listen(client_port, function () {
 });
