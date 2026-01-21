cb5f03d807b14f1c0d43492c45f08e8f81cd413b
PhoenixNels <phoenixnelson05@gmail.com>
2026-01-21 10:26:38 +0000

    Webhook Connection Setup

--- createWebhook.js
+++ createWebhook.js
@@ -1,8 +1,6 @@
 //initialising a webhook 
 
-
 require("dotenv").config();
-
 
 fetch(`https://api.figma.com/v2/webhooks`, {
     method: 'POST',
@@ -11,17 +9,17 @@ fetch(`https://api.figma.com/v2/webhooks`, {
         'X-Figma-Token': process.env.FIGMA_TOKEN,
     },
     body:JSON.stringify({
-        event_type: 'PING',
+        event_type: 'PING', //just a PING event so this just initialises it - will create another issue to let it log file updates later
         team_id: process.env.TEAM_ID,
         passcode: 'Verified',
         //file_key: process.env.FILE_ID,
-        endpoint: 'https://mean-webs-find.loca.lt/updates',
+        endpoint: 'https://yellow-dodo-11.loca.lt/updates', //our new webserver that figma can access - no longer http://localserver.....
         description: 'Webhook to test implementation'
 
     })
     
 })
-
+//prints out the response Figma gives us 
     .then(res => {
         console.log("Status:", res.status);
         return res.json();
