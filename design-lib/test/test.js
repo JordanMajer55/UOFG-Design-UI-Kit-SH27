6e37fa1d82f3b23bf64d3e9adf7f6ece46ab1f8d
Jordan Majer <jordanmajer55690@gmail.com>
2026-02-03 20:09:12 +0000

    refactor: removed not needed files

--- design-lib/test/test.js
+++
@@ -1,6 +0,0 @@
-import { styleButtonByClass } from "./node_modules/design-lib/dist/index.js";
-
-console.log(styleButtonByClass);
-// styleButtonByClass("main", "primary");
-
-document.getElementsByClassName("main")[0].style.backgroundColor="red";
