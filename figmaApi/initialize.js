b39783e8f119ead844f9ec0274b17305ce7d12c7
Otgonbayar Bayansan <2963377b@student.gla.ac.uk>
2026-02-11 00:41:40 +0000

    feat: add phase banner token generator

--- figmaApi/initialize.js
+++ figmaApi/initialize.js
@@ -1,5 +1,6 @@
 const generateButtonTokens = require("./initialisation/generateButtonTokens.js");
 const generateSearchBoxTokens = require("./initialisation/generateSearchBoxTokens.js");
+const generatePhaseBannerTokens = require("./initialisation/generatePhaseBannerTokens.js")
 
 async function init() {
   const BUTTON_VARIANTS = [
@@ -22,9 +23,18 @@ async function init() {
       fileName: "SearchBox-Default"
     },
   ];
-  
+
+  const PHASEBANNER_VARIANTS = [
+    {
+      name: "beta",
+      nodeId: "43:349",
+      fileName: "PhaseBanner-Beta"
+    },
+  ];
+
   await generateButtonTokens(BUTTON_VARIANTS);
   await generateSearchBoxTokens(SEARCHBOX_VARIANTS);
+  await generatePhaseBannerTokens(PHASEBANNER_VARIANTS);
 }
 
 init();
