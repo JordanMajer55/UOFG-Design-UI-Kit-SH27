2fc46dc6b4e8f9063bff07248e24ee01cf01d1e4
Jordan Majer <jordanmajer55690@gmail.com>
2026-02-12 23:06:47 +0000

    feature: added function that finds path to a node in the full file

--- figmaApi/initialisation/generateTabsTokens.js
+++ figmaApi/initialisation/generateTabsTokens.js
@@ -1,6 +1,7 @@
 // Same logic as generateButtonTokens
 const fs = require("fs");
 const fetchNode = require("./figmaGetNode.js");
+const findNodeById = require("./findNode.js");
 
 function rgbaToCss(color) {
     const r = Math.round((color?.r ?? 0) * 255);
@@ -10,11 +11,14 @@ function rgbaToCss(color) {
     return `rgba(${r}, ${g}, ${b}, ${a})`;
 }
 
+function extractTabsToken(component) {
+    
+}
 
 async function generateTabsTokens(variants) {
     for (const variant of variants) {
-        const node = await fetchNode(variant.nodeId);
-        const component = node?.nodes?.[variant.nodeId]?.document;
+        var file = require("./fullFile.json");
+        const component = file.document.children[variant.position].children[variant.canvas].children[variant.frame];
 
         if (!component) {
             throw new Error(`No document found for nodeId=${variant.nodeId}`);
