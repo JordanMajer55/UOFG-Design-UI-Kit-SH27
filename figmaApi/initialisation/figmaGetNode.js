4a53557d62dfb7f681706833ef72214ed860f4a3
Jordan Majer <jordanmajer55690@gmail.com>
2026-04-05 16:56:00 +0100

    refactor: removed secret

--- figmaApi/initialisation/figmaGetNode.js
+++ figmaApi/initialisation/figmaGetNode.js
@@ -3,10 +3,6 @@ const { readFileSync } = require("node:fs");
 const findNodeById = require('./findNode');
 require("dotenv").config();
 
-
-const FILE_ID = "ZOFT3XTYEsdMVK3zkQ0PI9";// this needs to be moved to an env
-
-
 async function fetchNode(nodeId) {
   const fileData = JSON.parse(readFileSync("./../figmaApi/initialisation/fullFile.json"))
   const component = findNodeById(fileData.document, nodeId)
